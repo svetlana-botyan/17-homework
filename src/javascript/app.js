58f977265db3373300fed9237f699c1879e13e64
svetlana-botyan <svetlana.botian@gmail.com>
2021-12-06 22:22:16 +0300

    Ready task №3 and install Eslint

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -2,7 +2,7 @@ import "../scss/app.scss";
 
 import { Car } from "./car";
 import { Truck } from "./truck";
-import {SportCar } from './sport-car'
+import { SportCar } from "./sport-car";
 
 // №1
 const carBMV = new Car("BMV X6", 2019, 6.6);
@@ -15,11 +15,10 @@ console.log("Расход топлива: " + carBMV.calcFuel(distanse));
 const carVolvo = new Truck("Volvo XC 90", 2019, 7.7, 4);
 carVolvo.getInfo();
 
-console.log("Расход топлива: " + carVolvo.calcFuel(distanse)
-);
+console.log("Расход топлива: " + carVolvo.calcFuel(distanse));
 
-carVolvo.addCargo(8)
-console.log(carVolvo.cargoCabin) //вывод массив
+carVolvo.addCargo(8);
+console.log(carVolvo.cargoCabin); //вывод массив
 
 // №3
 const carFerrari = new SportCar("Ferrari Roma", 2020, 9.7, 320);
@@ -27,7 +26,9 @@ carFerrari.getInfo();
 
 console.log("Расход топлива: " + carFerrari.calcFuel(distanse));
 
-console.log(carFerrari.start())
+console.log(carFerrari.start()); //завели машину
 
-const value = 250
-carFerrari.speedUp(value)
+const value = 250;
+carFerrari.speedUp(value);
+
+console.log(carFerrari.currentSpeed + " км/ч");
--- src/javascript/sport-car.js
+++ src/javascript/sport-car.js
@@ -4,16 +4,25 @@ class SportCar extends Car {
   currentSpeed = 0;
   isWorking = false;
 
-  constructor(brand, year, fuelRate, distance, maxSpeed) {
-    super(brand, year, fuelRate, distance);
+  constructor(brand, year, fuelRate, maxSpeed) {
+    super(brand, year, fuelRate);
     this.maxSpeed = maxSpeed;
   }
 
   start() {
     this.isWorking = true;
-    return `${this.brand} - заведена`
+    return `${this.brand} - заведена`;
   }
 
-  speedUp(value) {}
+  speedUp(value) {
+    if (this.isWorking == true) {
+      this.currentSpeed += value;
+
+      if (this.currentSpeed > this.maxSpeed) {
+        console.error("не работает");
+      }
+    } else alert("Нужно завести машину");
+  }
 }
+
 export { SportCar };
